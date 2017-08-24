9b139ff833c955d5228ab7033f741cc078989727
Alumno <Alumno@LAB-BD-01>
2017-08-24 08:39:15 -0500

    Act 5 Hola Mundo

---
+++ src/java/Mundito.java
@@ -0,0 +1,86 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Altaír Sanchez
+ */
+
+    //redirige el navegador a la dirección del servlet
+    @WebServlet(urlPatterns = {"/Mundito"})
+public class Mundito extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException si un error sucede. 
+     */
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            /* Codigo para la visualización del cuerpo */
+            
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            /* Titulo del servlet */
+            out.println("<title>Servlet Mundito</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            /* Impresión de texto en la pantalla */
+            out.println("<h1>¡Hola Mundo!</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return ":D";
+    }// </editor-fold>
+
+}
